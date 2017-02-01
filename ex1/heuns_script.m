2b806c82715c3b31a45ae90753593e22511902b0
Xav Kearney <xavkearney@gmail.com>
2017-02-01 11:12:52 +0000

    Added the different input voltage functions to the heuns script, and tweaked the graph plotting settings.

--- ex1/heuns_script.m
+++ ex1/heuns_script.m
@@ -1,10 +1,69 @@
 R = 0.5; %set resistor and inductor values
 L = 1.5e-3;
 t0 = 0; %set start and finish times
-tf = 0.020;
-f = @(t) 5; %define the input voltage function
 N = 100; %set number of intervals
 i0 = 0; %initial condition of current
+method = 'Heuns'; %define the method (for the figure window titles)
 
+%INPUT VOLTAGE f = 5 (step function)
+name = 'Vin = step(5V)'
+tf = 0.020; %set the time period
+f = @(t) 5; %define the input voltage as a function of time
+[t,vout] = heuns(f,t0,tf,N,i0,L,R) %get the output voltage array
+vin=arrayfun(f,t); %calculate the input voltages
+figure('Name',[method ': ' name],'NumberTitle','off'); %create a figure with a descriptive window title
+plot(t,vin,'--') %plot input voltages
+hold on; %wait for the second figure
+plot(t,vout,'*') %plot the output voltages
+title(name) %add a title
+hold off;
+legend('Vin','Vout'); %add a legend to the plot
+
+%INPUT VOLTAGE f = impulsive signal
+name = 'Vin = 4V Impulse'
+tf = 0.035;
+f = @(t) 4*exp(-(t^2)/140e-6);
 [t,vout] = heuns(f,t0,tf,N,i0,L,R)
+vin=arrayfun(f,t);
+figure('Name',[method ': ' name],'NumberTitle','off');
+plot(t,vin,'--')
+hold on;
 plot(t,vout,'*')
+title(name)
+hold off;
+legend('Vin','Vout');
+
+%INPUT VOLTAGE f = decay signal
+name = 'Vin = 4V Exponential Decay'
+tf = 0.01;
+f = @(t) 4*exp(-(t)/140e-6);
+[t,vout] = heuns(f,t0,tf,N,i0,L,R)
+vin=arrayfun(f,t);
+figure('Name',[method ': ' name],'NumberTitle','off');
+plot(t,vin,'--')
+hold on;
+plot(t,vout,'*')
+title(name)
+hold off;
+legend('Vin','Vout');
+
+%INPUT VOLTAGE f = sine wave
+name = 'Vin = Sine, A=5, T=140\mus'
+tf = 140e-6;
+A=5; %set amplitude
+T=140e-6; %set time period
+f = @(t) A*sin(t*2*pi/T);
+[t,vout] = heuns(f,t0,tf,N,i0,L,R)
+vin=arrayfun(f,t);
+figure('Name',[method ': ' name],'NumberTitle','off');
+plot(t,vin,'--')
+hold on;
+plot(t,vout,'*')
+title(name)
+hold off;
+legend('Vin','Vout');
+
+
+
+
+
